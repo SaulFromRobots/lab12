abeeb27cba555414437c3624c50c15989e30b91a
SaulFromRobots <saulrs951@protonmail.com>
2025-11-14 11:44:21 -0800

    oops, forgot parent traversal.

--- trees/FamilyTree.java
+++ trees/FamilyTree.java
@@ -54,6 +54,11 @@ public class FamilyTree {
 			// the nodes of a tree is like traversing a linked list. If that isn’t clear,
 			// draw a tree, mark any leaf node, and then mark its ancestors in order from
 			// recent to ancient. Expect a question about this on the final exam.
+			TreeNode pointer = this;
+			while (pointer != null) {
+				ancestors.add(pointer.name);
+				pointer = pointer.parent;
+			}
 
 			return ancestors;
 		}
